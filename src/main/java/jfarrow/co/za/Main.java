2db0aa694e304a46700196e5e3f4d26dcc24b5c0
Justin <jfarrow023@student.wethinkcode.co.za>
2024-09-25 22:21:06 +0200

    Imported bencoding module, credit to gyurix @https://bitbucket.org/gyuriX/bencoder/src/master/

--- src/main/java/jfarrow/co/za/Main.java
+++ src/main/java/jfarrow/co/za/Main.java
@@ -1,7 +1,22 @@
 package jfarrow.co.za;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import gyurix.bencoder.BEncoder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (Scanner scan = new Scanner(System.in)) {
+            BEncoder be = new BEncoder();
+            System.out.print("Enter Encoded Data: ");
+            String in = scan.nextLine();
+            be.setInput(in);
+            ArrayList<Object> list = be.readAll();
+            System.out.println("Decoded:\n" + list);
+            be.resetOutput();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
